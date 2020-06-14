5da1519fea7de886ec3eaf764115bae01756e85f
hiep294 <quanghiep294@gmail.com>
2020-06-14 22:43:42 +0700

    ss

--- src/Content/TodoAreaExample/index.tsx
+++ src/Content/TodoAreaExample/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Switch, useRouteMatch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import List from './pages/List';
 
 export default () => {
-  const match = useRouteMatch();
+  // const match = useRouteMatch();
+  // => should not use match, it is not friendly for search global
   return (
     <Switch>
-      <Route path={`${match.url}`} component={List} />
+      <Route path="/todos" component={List} />
     </Switch>
   );
 };
